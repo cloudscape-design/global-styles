a502340c1e569c73f858b1ec779377752cc318ac
Michael Dowse <dowsem@amazon.com>
2024-11-27 13:09:30 +0100

    chore: Migrate from jest to vitest

--- src/__tests__/disable-motion.test.ts
+++ src/__tests__/disable-motion.test.ts
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import { test, expect, beforeEach, afterEach } from 'vitest';
 import { disableMotion } from '../index';
 
 let originalClassName: string;
